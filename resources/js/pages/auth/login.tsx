52a93e9773a6a9d584c0dbb91cec059a6ff45f8f
haykal-fe-verd <haykal@verd.net.id>
2025-05-28 10:00:22 +0700

    fix: typescript

--- resources/js/pages/auth/login.tsx
+++ resources/js/pages/auth/login.tsx
@@ -1,19 +1,19 @@
-import React from "react";
-import { Link, useForm } from "@inertiajs/react";
-import { Eye, EyeOff, Loader2 } from "lucide-react";
+import React from 'react';
+import { Link, useForm } from '@inertiajs/react';
+import { Eye, EyeOff, Loader2 } from 'lucide-react';
 
-import { cn } from "@/lib/utils";
+import { cn } from '@/lib/utils';
 
-import GuestLayout from "@/layouts/guest-layout";
-import { Icons } from "@/components/icons";
-import { Card, CardContent } from "@/components/ui/card";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
-import InputError from "@/components/input-error";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Button } from "@/components/ui/button";
+import GuestLayout from '@/layouts/guest-layout';
+import { Icons } from '@/components/icons';
+import { Card, CardContent } from '@/components/ui/card';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import InputError from '@/components/input-error';
+import { Checkbox } from '@/components/ui/checkbox';
+import { Button } from '@/components/ui/button';
 
-import Cover from "@/assets/img/7.jpeg";
+import Cover from '@/assets/img/7.jpeg';
 
 interface LoginProps {
     canResetPassword: boolean;
@@ -22,8 +22,8 @@ interface LoginProps {
 function Login({ canResetPassword }: LoginProps) {
     // hooks
     const { data, setData, post, processing, errors, reset } = useForm({
-        email: "",
-        password: "",
+        email: '',
+        password: '',
         remember: false,
     });
 
@@ -31,11 +31,11 @@ function Login({ canResetPassword }: LoginProps) {
     const [showPassword, setShowPassword] = React.useState<boolean>(false);
 
     // events
-    const onSubmit: React.FormEventHandler = async (e) => {
+    const onSubmit: React.FormEventHandler = async e => {
         e.preventDefault();
 
-        post(route("login"), {
-            onFinish: () => reset("password"),
+        post(route('login'), {
+            onFinish: () => reset('password'),
         });
     };
 
@@ -43,7 +43,7 @@ function Login({ canResetPassword }: LoginProps) {
         <GuestLayout title="Login">
             <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
                 <div className="w-full max-w-sm md:max-w-5xl">
-                    <div className={cn("flex flex-col gap-6")}>
+                    <div className={cn('flex flex-col gap-6')}>
                         <Card className="overflow-hidden">
                             <CardContent className="grid p-0 md:grid-cols-2">
                                 <div className="relative hidden bg-muted md:block">
@@ -56,13 +56,10 @@ function Login({ canResetPassword }: LoginProps) {
                                     </div>
                                 </div>
 
-                                <form
-                                    onSubmit={onSubmit}
-                                    className="p-6 md:p-8"
-                                >
+                                <form onSubmit={onSubmit} className="p-6 md:p-8">
                                     <div className="flex flex-col gap-6">
                                         <div className="flex flex-col items-center text-center">
-                                            <Link href={route("home")}>
+                                            <Link href={route('home')}>
                                                 <Icons.Logo className="h-24 w-auto fill-primary" />
                                             </Link>
 
@@ -70,8 +67,7 @@ function Login({ canResetPassword }: LoginProps) {
                                                 Login
                                             </h1>
                                             <p className="mb-5 text-sm lg:text-base text-center">
-                                                Selamat Datang Di{" "}
-                                                {import.meta.env.VITE_APP_NAME}
+                                                Selamat Datang Di {import.meta.env.VITE_APP_NAME}
                                             </p>
                                         </div>
 
@@ -84,40 +80,24 @@ function Login({ canResetPassword }: LoginProps) {
                                                 placeholder="jhon@doe.com"
                                                 value={data.email}
                                                 autoComplete="username"
-                                                onChange={(e) =>
-                                                    setData(
-                                                        "email",
-                                                        e.target.value
-                                                    )
-                                                }
+                                                onChange={e => setData('email', e.target.value)}
                                             />
-                                            <InputError
-                                                message={errors.email}
-                                            />
+                                            <InputError message={errors.email} />
                                         </div>
 
                                         {/* password */}
                                         <div className="grid gap-2">
-                                            <Label htmlFor="password">
-                                                Password
-                                            </Label>
+                                            <Label htmlFor="password">Password</Label>
 
                                             <div className="relative">
                                                 <Input
                                                     id="password"
-                                                    type={
-                                                        showPassword
-                                                            ? "text"
-                                                            : "password"
-                                                    }
+                                                    type={showPassword ? 'text' : 'password'}
                                                     placeholder="••••••••"
                                                     autoComplete="current-password"
                                                     value={data.password}
-                                                    onChange={(e) =>
-                                                        setData(
-                                                            "password",
-                                                            e.target.value
-                                                        )
+                                                    onChange={e =>
+                                                        setData('password', e.target.value)
                                                     }
                                                 />
 
@@ -125,20 +105,16 @@ function Login({ canResetPassword }: LoginProps) {
                                                     type="button"
                                                     aria-label={
                                                         showPassword
-                                                            ? "Sembunyikan password"
-                                                            : "Tampilkan password"
+                                                            ? 'Sembunyikan password'
+                                                            : 'Tampilkan password'
                                                     }
                                                     title={
                                                         showPassword
-                                                            ? "Sembunyikan password"
-                                                            : "Tampilkan password"
+                                                            ? 'Sembunyikan password'
+                                                            : 'Tampilkan password'
                                                     }
                                                     className="absolute inset-y-0 right-0 bg-primary p-3 flex items-center text-primary-foreground rounded-r-md"
-                                                    onClick={() =>
-                                                        setShowPassword(
-                                                            !showPassword
-                                                        )
-                                                    }
+                                                    onClick={() => setShowPassword(!showPassword)}
                                                 >
                                                     {showPassword ? (
                                                         <Eye className="w-4 h-4" />
@@ -147,9 +123,7 @@ function Login({ canResetPassword }: LoginProps) {
                                                     )}
                                                 </button>
                                             </div>
-                                            <InputError
-                                                message={errors.password}
-                                            />
+                                            <InputError message={errors.password} />
                                         </div>
 
                                         <div className="flex gap-2 justify-between">
@@ -157,9 +131,8 @@ function Login({ canResetPassword }: LoginProps) {
                                                 <Checkbox
                                                     id="remember"
                                                     name="remember"
-                                                    onCheckedChange={(e) =>
-                                                        setData("remember", e)
-                                                    }
+                                                    // @ts-expect-error: CheckedState includes "indeterminate", but we only need boolean
+                                                    onCheckedChange={e => setData('remember', e)}
                                                     checked={data.remember}
                                                 />
                                                 <label
@@ -172,9 +145,7 @@ function Login({ canResetPassword }: LoginProps) {
 
                                             {canResetPassword && (
                                                 <Link
-                                                    href={route(
-                                                        "password.request"
-                                                    )}
+                                                    href={route('password.request')}
                                                     className="ml-auto text-sm underline-offset-2 hover:underline"
                                                 >
                                                     Lupa password?
@@ -184,9 +155,9 @@ function Login({ canResetPassword }: LoginProps) {
 
                                         <div className="flex gap-2 justify-between">
                                             <div className="text-xs font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                                                Belum punya akun?{" "}
+                                                Belum punya akun?{' '}
                                                 <Link
-                                                    href={route("register")}
+                                                    href={route('register')}
                                                     className="underline hover:text-primary"
                                                 >
                                                     Daftar disini
@@ -199,9 +170,7 @@ function Login({ canResetPassword }: LoginProps) {
                                             className="w-full inline-flex items-center justify-center gap-3"
                                             disabled={processing}
                                         >
-                                            {processing && (
-                                                <Loader2 className="animate-spin" />
-                                            )}
+                                            {processing && <Loader2 className="animate-spin" />}
                                             <span>Login</span>
                                         </Button>
                                     </div>
